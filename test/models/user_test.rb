7997be6dea7e03a53758d090b602357c057556ea
mulhoo <oliviamulholland94@gmail.com>
2020-05-25 13:18:56 -0700

    user back to where it was

---
+++ test/models/user_test.rb
@@ -0,0 +1,62 @@
+require "test_helper"
+
+describe User do
+  let (:new_user) {
+    new_user = User.new(username: "Light Yagami")
+  }
+
+  it "can be instantiated" do
+    expect(new_user.valid?).must_equal true
+  end
+
+  it "has required fields" do
+    new_user.save
+    user = User.first
+    [:username].each do |field|
+      expect(user).must_respond_to field
+    end
+  end
+
+  describe "relationships" do
+    it "has many votes" do
+      new_user.save
+      user = User.first
+
+      expect(user.votes.count).must_be :>=, 0
+      user.votes.each do |vote|
+        expect(vote).must_be_instance_of Vote
+      end
+    end
+  end
+
+  describe "validations" do
+    it "requires a username" do
+      new_user.username = nil
+
+      expect(new_user.valid?).must_equal false
+      expect(new_user.errors.messages).must_include :username
+      expect(new_user.errors.messages[:username]).must_equal ["can't be blank"]
+    end
+  end
+
+  describe "custom method - works_by_user" do
+    before do
+      @work_1 = Work.create category: "book", title: "DeathNote", creator: "Tsugumi Ohba", publication_year: 2003, description: "angsty teenage boy plays god"
+      @work_2 = Work.create category: "album", title: "Future Nostalgia", creator: "Dua Lipa", publication_year: 2020, description: "Dua Lipa's debut album"
+
+      @user_1 = User.create username: "Light Yagami"
+      @user_2 = User.create username: "L"
+      
+      @vote_1 = Vote.create user_id: @user_1.id, work_id: @work_1.id
+      @vote_2 = Vote.create user_id: @user_2.id, work_id: @work_1.id
+      @vote_3 = Vote.create user_id: @user_1.id, work_id: @work_2.id
+    end
+
+    it "shows work by user" do
+      works = @user_1.works_by_user
+      expect(works[0].id).must_equal @work_1.id
+      expect(works[1].id).must_equal @work_2.id
+    end
+  end
+end
+
