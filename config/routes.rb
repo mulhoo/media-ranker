75c856bb4d9e64efe38b6f78505ffd07ac713d29
mulhoo <oliviamulholland94@gmail.com>
2020-05-25 13:45:53 -0700

    routes to where they were

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'featured#index'
+  resources :works
+  resources :users
+  
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+  
+  get '/vote/:id', to: 'votes#vote_by_work_id', as: 'vote_by_work_id'
+  get '/featured', to: 'featured#index'
 end
