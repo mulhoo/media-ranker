e833a84d153e5a41352f541d6dbc7424945defbe
mulhoo <oliviamulholland94@gmail.com>
2020-05-26 15:15:27 -0700

    fixed create vote bug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,22 +1,28 @@
 class VotesController < ApplicationController
   def create
-    work = Work.find_by(id: params[:id])
-    user = User.find_by(id: params[:id])
+    if session[:user_id].nil?
+      flash[:warning] = "You must be logged in to Vote"
+      redirect_to work_path
+      return
+    end
 
-    if work.nil?
+    @work = Work.find(params[:id])
+    @user = User.find(session[:user_id])
+
+    if @work.nil?
       head :not_found
       return
     end
 
-    vote = Vote.new(work: work, user: user)
+    vote = Vote.new(work: @work, user: @user)
 
     if vote.save
       flash[:success] = "Upvote Successful"
-      redirect_to work_path(work.id)
+      redirect_to work_path(@work.id)
       return
     else
       flash[:warning] = "Could not upvote"
-      redirect_to work_path(work.id)
+      redirect_to work_path(@work.id)
       return
     end
   end
